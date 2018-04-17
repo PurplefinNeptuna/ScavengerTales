40d905ca74881d8f611896b3c9c383934ae858ba
Ilham AJ <ilhamaryasuta@gmail.com>
2018-04-17 14:57:45 +0700

    initial commit

---
+++ Assets/Scripts/GameEvent.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent : MonoBehaviour {
+	public void DoQuit(){
+		Debug.Log ("Quit Success");
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/ScrollRectVertSnap.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollRectVertSnap : MonoBehaviour {
+
+}
