56b4e8545546aa0916cb0d842188ae561fb6cc3e
Ilham AJ <ilhamaryasuta@gmail.com>
2020-09-01 16:45:47 +0700

    new commit

---
+++ Assets/Resources/Scripts/Craft.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient{
+	public String name;
+	public int qty;
+	public Ingredient(String name, int qty) {
+		this.name = name;
+		this.qty = qty;
+	}
+}
+
+public class Craft {
+	public int resultQty;
+	public String resultName;
+	private Ingredient[] ingredients;
+
+	#region UI
+	private String displayName;
+	private GameObject craftUIObject;
+	private CraftList craftList;
+	private PanelController panel;
+	#endregion
+
+	public Craft(String resultName, int resultQty, Ingredient[] ingredients) {
+		this.resultName = resultName;
+		this.resultQty = resultQty;
+		this.ingredients = ingredients;
+		displayName = GameCore.main.GetItem(resultName).displayName;
+	}
+
+	public void UIStart() {
+		craftList = new CraftList(resultName, displayName);
+		craftUIObject = craftList.Start();
+		panel = craftUIObject.GetComponent<PanelController>();
+		panel.ButtonClicker += CraftThis;
+	}
+
+	public void UIUpdate() {
+		craftList.Update();
+	}
+
+	public void CraftThis() {
+		bool craftAble = true;
+		
+		foreach (Ingredient i in ingredients) {
+			if (GameCore.main.GetItem(i.name).count < i.qty)
+				craftAble = false;
+		}
+
+		if (craftAble) {
+			foreach (Ingredient i in ingredients) {
+				GameCore.main.GetItem(i.name).count -= i.qty;
+			}
+			GameCore.main.GetItem(resultName).count += resultQty;
+		}
+		craftList.Update();
+	}
+}
---
+++ Assets/Resources/Scripts/PanelController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PanelController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler {
+
+	public delegate void OnButtonClick();
+	public event OnButtonClick ButtonClicker;
+
+	public delegate void OnUpdate();
+	public event OnUpdate Updater;
+
+	public delegate void OnPanelClick();
+	public event OnPanelClick PanelClicker;
+
+	public string spriteName;
+	public string Name;
+	public string ButtonText;
+	public string Description;
+
+	private GameObject[] children;
+	private GameObject PanelImage;
+	private GameObject PanelName;
+	private GameObject PanelDescription;
+	private GameObject PanelButton;
+
+	private void Awake() {
+		children = GetComponentsInChildren<Transform>(true).Where(i => i.parent == transform).Select(i => i.gameObject).ToArray();
+		PanelImage = children.SingleOrDefault(i => i.name == "PanelImage");
+		PanelName = children.SingleOrDefault(i => i.name == "PanelName");
+		PanelDescription = children.SingleOrDefault(i => i.name == "PanelDescription");
+		PanelButton = children.SingleOrDefault(i => i.name == "PanelButton");
+	}
+	// Use this for initialization
+	void Start() {
+		PanelImage.GetComponent<Image>().sprite = GameCore.main.GetSprite(spriteName);
+		PanelName.GetComponent<Text>().text = Name;
+		PanelButton.GetComponentInChildren<Text>();
+	}
+	// Update is called once per frame
+	void Update() {
+		PanelDescription.GetComponent<Text>().text = Description;
+		PanelButton.GetComponentInChildren<Text>(true).text = ButtonText ?? "Test";
+		if (Updater != null)
+			Updater();
+	}
+
+	public void ButtonClick() {
+		if (ButtonClicker != null)
+			ButtonClicker();
+	}
+
+	public void OnPointerClick(PointerEventData e) {
+		if (PanelClicker != null)
+			PanelClicker();
+	}
+
+	public void OnPointerDown(PointerEventData e) {
+	}
+
+	public void OnPointerUp(PointerEventData e) {
+	}
+}
